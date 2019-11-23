3aa9b9a9fbdc50f0ebed3b24f44e343e47ca4a8c
Ilyas Ndhote <ii.ndhote@gmail.com>
2019-11-23 11:48:18 +0300

    solved the cors error

--- src/app/connection.service.ts
+++ src/app/connection.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from "@angular/core";
   providedIn: "root"
 })
 export class ConnectionService {
-  url: string = "http://localhost:8080/send";
+  url: string = "/send";
   constructor(private http: HttpClient) {}
 
   sendMessage(messageContent: any) {
